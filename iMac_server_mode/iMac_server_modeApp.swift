3e46d5cbdba3deceae3c6439ad0dc0f7103a42f8
Naphlk <l.k.naph@gmail.com>
2023-10-25 21:40:14 -0400

    The first commit

---
+++ iMac_server_mode/iMac_server_modeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMac_server_modeApp.swift
+//  iMac_server_mode
+//
+//  Created by viettronics868 on 2023-10-25.
+//
+
+import SwiftUI
+
+@main
+struct iMac_server_modeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
